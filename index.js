db9c7d0aec62c5a344c038ea10b1da0502381b1d
SumithAzhiValappil <79736839+SumithAzhiValappil@users.noreply.github.com>
2023-02-14 23:46:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ async function handleRequest(request) {
         if (request.cf.country != 'SG') {
         return Response.redirect('https://1.1.1.1');
         }
-    return new Response("“This is your "+request.headers.get('CF-Connecting-IP')+"and you are accessing this site from "+request.cf.country+" | "+request.cf.asn);
+    return new Response("This is your "+request.headers.get('CF-Connecting-IP')+" and you are accessing this site from "+request.cf.country+" | "+request.cf.asn);
 }
 
 addEventListener("fetch", event => {
